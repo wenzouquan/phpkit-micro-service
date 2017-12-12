8c6e0910719a03bb0db4224b799ca77681f269ec
DFRobot <dfrobot@suyudeMacBook-Pro-2.local>
2017-12-12 10:46:55 +0800

    服务ip不定义情况处理

--- Client.php
+++ Client.php
@@ -12,10 +12,12 @@ class Client{
     }
 
     function getService($serverName="",$ip="",$port=""){
-        $key = array_rand($this->serverips, 1); //随机找到一个服务
-        $ipAndPort = explode(":",$this->serverips[$key]);
-        $this->service = $this->client->getRPCService("Services\\Demo\\HiService",$ipAndPort[0],$ipAndPort[1]);
         $this->className = $serverName;
+        if(is_array($this->serverips) && !empty($this->serverips)){
+            $key = array_rand($this->serverips, 1); //随机找到一个服务
+            $ipAndPort = explode(":",$this->serverips[$key]);
+            $this->service = $this->client->getRPCService("Services\\Demo\\HiService",$ipAndPort[0],$ipAndPort[1]);
+        }
         return $this;
     }
 
@@ -28,10 +30,12 @@ class Client{
         );
         if(class_exists($this->className)){
             $returnMsg= call_user_func_array(array(new $this->className(), $name), $arguments);//如果服务在本地
-        }else{
+        }else if($this->service){
             $returnMsg= json_decode($this->service->say(json_encode($params)),true);
+        }else{
+            throw new \Exception($this->className() ." 类未定义 ", 1);
+            
         }
-
         return $returnMsg;
     }
 
